7df09d7ab73485f63b01b078a3a776e9e87dfe9b
Karly <karlyhoffman@gmail.com>
2018-10-06 10:48:13 -0500

    Initial create-react-app build

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./assets/styles/main.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="portfolio-template-4">
+        <h1>Hello, New Portfolio.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
