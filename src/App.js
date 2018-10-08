4f660a0e1812f08db54b6a51d853452b0d15908a
Karly <karlyhoffman@gmail.com>
2018-10-08 12:36:56 -0500

    add fonts

--- src/App.js
+++ src/App.js
@@ -28,23 +28,85 @@ class App extends Component {
                 updateSection={this.handleSectionUpdate}>
 
                     <section className="animate">
-                        <h1>Hello, New Portfolio.</h1>
+                        <h1>Karly Hoffman</h1>
+                        <p className="subhead">Chicago based Front End Developer.</p>
                     </section>
 
                     <section>
-                        <h1>Section 1</h1>
+                        <h2>Web editor turned web developer</h2>
+                        <p>Gained experience writing and organizing content for web pages, now brings that content to life through programming.</p>
+                        <p>Has an eye for detail and the ability to add functionality to any page or application.</p>
+                        <p>Knowledgeable, dependable, and versatile in the web industry.</p>
                     </section>
 
                     <section>
-                        <h1>Section 2</h1>
+                        <h2>Expertise</h2>
+                        <ul>
+                            <li>UI Development</li>
+                            <li>JavaScript</li>
+                            <li>CSS/SCSS</li>
+                            <li>Animations</li>
+                            <li>Vue</li>
+                            <li>React</li>
+                        </ul>
                     </section>
 
                     <section>
-                        <h1>Section 3</h1>
+                        <h2>Notable Projects</h2>
+                        <div className="project-desc">
+                            <h3 className="title">McDonald's Visual Design Hub</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere a nisi sed mattis. Suspendisse potenti. Duis et consectetur purus. Phasellus cursus rutrum ex vitae lacinia.</p>
+                            <p>Technologies Used</p>
+                            <ul>
+                                <li>React</li>
+                                <li>GSAP</li>
+                                <li>WordPress</li>
+                            </ul>
+                        </div>
                     </section>
 
                     <section>
-                        <h1>Section 4</h1>
+                        <h2>Notable Projects</h2>
+                        <div className="project-desc">
+                            <h3 className="title">National Geographic</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere a nisi sed mattis. Suspendisse potenti. Duis et consectetur purus. Phasellus cursus rutrum ex vitae lacinia.</p>
+                            <p>Technologies Used</p>
+                            <ul>
+                                <li>React</li>
+                                <li>GSAP</li>
+                                <li>WordPress</li>
+                            </ul>
+                        </div>
+                    </section>
+
+                    <section>
+                        <h2>Notable Projects</h2>
+                        <div className="project-desc">
+                            <h3 className="title">Good Reason Houston</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere a nisi sed mattis. Suspendisse potenti. Duis et consectetur purus. Phasellus cursus rutrum ex vitae lacinia.</p>
+                            <p>Technologies Used</p>
+                            <ul>
+                                <li>Wordpress</li>
+                                <li>GSAP</li>
+                            </ul>
+                        </div>
+                    </section>
+
+                    <section>
+                        <h2>Notable Projects</h2>
+                        <div className="project-desc">
+                            <h3 className="title">New Typo Graphics</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere a nisi sed mattis. Suspendisse potenti. Duis et consectetur purus. Phasellus cursus rutrum ex vitae lacinia.</p>
+                            <p>Technologies Used</p>
+                            <ul>
+                                <li>Vue</li>
+                                <li>GSAP</li>
+                            </ul>
+                        </div>
+                    </section>
+
+                    <section>
+                        <h2>Contact</h2>
                     </section>
             </AnimationContainer>
         )
