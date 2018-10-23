0700c20a97719393294f74e7b6294f8ef4fbae4b
Karly <karlyhoffman@gmail.com>
2018-10-23 15:51:11 -0700

    animations — timing tweaks

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
             projects: [
                 {
                     title: "McDonald's Visual Design Hub",
+                    link: null,
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere a nisi sed mattis. Suspendisse potenti. Duis et consectetur purus. Phasellus cursus rutrum ex vitae lacinia.",
                     technologies: [
                         "React",
@@ -31,21 +32,38 @@ class App extends Component {
                 },
                 {
                     title: "National Geographic | Hennessey",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere a nisi sed mattis. Suspendisse potenti. Duis et consectetur purus. Phasellus cursus rutrum ex vitae lacinia.",
+                    link: "https://www.nationalgeographic.com/hennessy/",
+                    description: "A responsive website showcasing artists and their Hennessy X.O. inspired artwork.",
                     technologies: [
                         "Vue",
-                        "CSS/SCSS",
+                        "SCSS",
                         "GSAP"
+                    ],
+                    deliverables: [
+                        "Parallax animations",
+                        "Navigation map",
+                        "Detail page templates",
+                        "Lightbox photo gallery",
+                        "Google Analytics",
+                        "Responsive design",
                     ],
                     image: natgeoImg
                 },
                 {
                     title: "Good Reason Houston",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere a nisi sed mattis. Suspendisse potenti. Duis et consectetur purus. Phasellus cursus rutrum ex vitae lacinia.",
+                    link: "https://goodreasonhouston.org/",
+                    description: "A WordPress website for a non-profit organization.",
                     technologies: [
-                        "CSS/SCSS",
+                        "SCSS",
                         "Wordpress",
+                        "PHP",
                         "GSAP"
+                    ],
+                    deliverables: [
+                        "CMS compatibility",
+                        "Full-page locking",
+                        "Transitional animations",
+                        "Responsive design",
                     ],
                     image: goodReasonImg
                 }
