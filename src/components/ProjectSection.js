9d408e386aa0f18af55bd82e85f7d0788566bea5
Karly <karlyhoffman@gmail.com>
2018-11-04 13:25:13 -0600

    styling

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -5,31 +5,33 @@ const ProjectSection = ({ title, description, technologies, image }) => {
 
     return (
         <section className="project">
-            <h2>Work</h2>
-            <div className="project-desc">
-                <SplitString 
-                    elementTag="h3"
-                    className="title">
-                        { title }
-                </SplitString>
-                <p className="appear">{ description }</p>
-                <h4 className="appear">Technologies Used:</h4>
-                <ul>
-                    {technologies.map((technology, idx) => (
-                        <li 
-                            className="appear" 
-                            key={idx}>
-                                { technology}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <div className="copy-container">
+                <h2>Work</h2>
+                <div className="project-desc">
+                    <SplitString
+                        elementTag="h3"
+                        className="title">
+                        {title}
+                    </SplitString>
+                    <p className="appear">{description}</p>
+                    <h4 className="appear">Technologies Used:</h4>
+                    <ul>
+                        {technologies.map((technology, idx) => (
+                            <li
+                                className="appear"
+                                key={idx}>
+                                {technology}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-            <div className="img-container"
-                style={{ 
-                    backgroundImage: `url(${image})`
-                }}
-            />
+                <div className="img-container"
+                    style={{
+                        backgroundImage: `url(${image})`
+                    }}
+                />
+            </div>
         </section>
     )
 }
