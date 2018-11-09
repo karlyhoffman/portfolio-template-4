49bac94cb1fc8c8cc31a0fb9c9b855233902186c
Karly <karlyhoffman@gmail.com>
2018-11-08 19:42:15 -0600

    Contact Section — add links + styling

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -7,12 +7,60 @@ const ContactSection = () => {
         <section id="contact">
             <SplitString elementTag="h2">Contact</SplitString>
             <ul>
-                <li className="appear">Email</li>
-                <li className="appear">Github</li>
-                <li className="appear">Linkedin</li>
-                <li className="appear">Codepen</li>
-                <li className="appear">Ello</li>
-                <li className="appear">Resume</li>
+                <li className="appear">
+                    <a 
+                        href="mailto:karlyhoffman@gmail.com" 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Email
+                    </a>
+                </li>
+                <li className="appear">
+                    <a
+                        href="https://github.com/karlyhoffman"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Github
+                    </a>
+                </li>
+                <li className="appear">
+                    <a
+                        href="https://www.linkedin.com/in/karlyhoffman/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Linkedin
+                    </a>
+                </li>
+                <li className="appear">
+                    <a
+                        href="https://codepen.io/karlyhoffman/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Codepen
+                    </a>
+                </li>
+                <li className="appear">
+                    <a
+                        href="https://ello.co/ello-govna"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Ello
+                    </a>
+                </li>
+                <li className="appear">
+                    <a
+                        href="https://karlyhoffman.github.io/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Resume
+                    </a>
+                </li>
             </ul>
         </section>
     )
