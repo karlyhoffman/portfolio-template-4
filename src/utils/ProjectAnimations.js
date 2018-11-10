5034b12aba7ecb034f650f0d83729e242c451dd5
Karly <karlyhoffman@gmail.com>
2018-11-09 19:04:09 -0600

    Project Section — convert stateless component to PureComponent

--- src/utils/ProjectAnimations.js
+++ src/utils/ProjectAnimations.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
+import fryImg from "../assets/images/mcd/fry.svg";
+import ketchupFryImg from "../assets/images/mcd/fry-ketchup.svg";
 import rockoImg from "../assets/images/hennessy/rocko-min.jpg";
 import fabricImg from "../assets/images/hennessy/fabric-min.jpg";
 import hennessyImg from "../assets/images/hennessy/hennessy-min.jpg";
@@ -7,29 +9,50 @@ import streetImg from "../assets/images/hennessy/street-min.jpg";
 import victorImg from "../assets/images/hennessy/victor-min.jpg";
 import yinImg from "../assets/images/hennessy/yin-min.jpg";
 
-const ProjectAnimations = ({ section }) => {
+class ProjectAnimations extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return <div className={`anim-container section-${section}`}>
-      <div id="hennessy">
-        <img src={rockoImg} alt="" />
-        <img src={fabricImg} alt="" />
-        <img src={hennessyImg} alt="" />
-        <img src={streetImg} alt="" />
-        <img src={victorImg} alt="" />
-        <img src={yinImg} alt="" />
-      </div>
+    this.state = {
+      section: this.props.section,
+    }
+  }
 
-      <div id="good-reason">
-        <div id="parent-circle">
-          <div className="circle blue" />
-          <div className="circle pink" />
-          <div className="circle lime" />
-          <div className="circle orange" />
-          <div className="circle teal" />
+  componentDidUpdate() {
+    this.setState({ section: this.props.section });
+  }
+
+  render() {
+    const { section } = this.state;
+
+    return (
+      <div className={`anim-container section-${section}`}>
+        <div id="mcd">
+          <img src={fryImg} alt="" />
+          <img src={ketchupFryImg} alt="" />
+        </div>
+
+        <div id="hennessy">
+          <img src={rockoImg} alt="" />
+          <img src={fabricImg} alt="" />
+          <img src={hennessyImg} alt="" />
+          <img src={streetImg} alt="" />
+          <img src={victorImg} alt="" />
+          <img src={yinImg} alt="" />
+        </div>
+
+        <div id="good-reason">
+          <div id="parent-circle">
+            <div className="circle blue" />
+            <div className="circle pink" />
+            <div className="circle lime" />
+            <div className="circle orange" />
+            <div className="circle teal" />
+          </div>
         </div>
       </div>
-    </div>;
-
+    )
+  }
 };
 
 export default ProjectAnimations;
